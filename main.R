1ff53ea31b9fd481cfd947850dfe4ef3e9f93849
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2018-06-15 11:38:45 +0200

    focus achats non stockés

--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library("readr")
 library("dplyr")
 library("tricky")
 library("readxl")
+library("tidyr")
 
 read_csv(
   file = "data-raw/tidy_ncc.csv"
@@ -14,14 +15,11 @@ read_csv(
   ) %>% 
   glimpse()
 
-read_csv(
-  file = "data-raw/tidy_ncc.csv", 
-  col_types = cols(numero = col_character())
-  ) %>% 
-  distinct(numero, libelle_long_complet_du_compte_cible) %>% 
-  datatable()
-
 
 table_cge <- read_excel(path = "data-raw/2012 - 2017_Balances des comptes de l'État.xlsx") %>% 
   set_standard_names() 
 
+table_cge %>% 
+  glimpse()
+
+table_cge 
